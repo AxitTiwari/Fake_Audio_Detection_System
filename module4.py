0dcc2aa417e2c1d5befd47f8caca1c2753d79e05
Shikhar Pundir <shikharpundir00@gmail.com>
2026-03-28 10:59:20 +0530

    Update module4.py

--- module4.py
+++ module4.py
@@ -51,10 +51,10 @@ X_train, X_test, y_train, y_test = train_test_split(
     X, y, test_size=0.2, random_state=42
 )
 
-train_ds = tf.data.Dataset.from_tensor_slices((X_train, y_train)) \
+train_ds = tf.data.Dataset.from_tensor_slices((X_train, y_train)) 
     .shuffle(1000).batch(BATCH_SIZE).prefetch(tf.data.AUTOTUNE)
 
-val_ds = tf.data.Dataset.from_tensor_slices((X_test, y_test)) \
+val_ds = tf.data.Dataset.from_tensor_slices((X_test, y_test)) 
     .batch(BATCH_SIZE).prefetch(tf.data.AUTOTUNE)
 
 
